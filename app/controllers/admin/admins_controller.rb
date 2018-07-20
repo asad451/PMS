652c10297d706bfc77bb8e9778cd6a59e8f30f3d
asad451 <asadmasood451@gmail.com>
2018-07-20 16:20:36 +0500

    Admin: Make existing user a Manager

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -8,11 +8,19 @@ class Admin::AdminsController < Admin::BaseController
     user = User.find(params[:id])
     if user.enable?
       user.disable!
-      sleep 1.5
+      sleep 1
+      redirect_to admin_root_path and return
+    elsif user.user?
+      user.manager!
+      sleep 1
+      redirect_to admin_root_path and return
+    elsif user.manager?
+      user.user!
+      sleep 1
       redirect_to admin_root_path and return
     else
       user.enable!
-      sleep 1.5
+      sleep 1
       redirect_to admin_root_path and return
     end  
   end
